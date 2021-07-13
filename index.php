d632a6d0939337e7dfd48644175782ab2e940abb
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-07-13 15:18:27 -0300

    criando classe page para configurar o Rain Tpl que será utilizando na renderização de páginas.

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->get('/', function(){
-    $sql = new Hcode\DB\sql();
+    $page = new Page();
+    $page->setTpl("index");
 
-    $results = $sql-> select("SELECT * FROM tb_users");
-
-    echo json_encode($results);
 });
 
 $app->run();
